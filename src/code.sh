b70dc34d940d5bd69ddbdb373a39258db7b45615
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-09-16 15:38:00 +0100

    Adding wheel files needed for igv-reports

--- src/code.sh
+++ src/code.sh
@@ -1,48 +1,28 @@
 #!/bin/bash
 
 main() {
+    set -exo pipeline 
+    export PATH=$PATH:/home/dnanexus/.local/bin  # pip installs some packages here, add to path
 
-    echo "Value of sites: '$sites'"
-    echo "Value of reference_genome: '$reference_genome'"
-    echo "Value of reference_index: '$reference_index'"
+    mark-section "Download the input files"
+    mkdir bam
+    mkdir references
+    dx-download-all-inputs --parallel
+    # BAMs need to be in same directory as their index
+    find ~/in/bam_file -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/bam
+    find ~/in/bam_index -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/bam
+    # Put reference material in one dir
+    find ~/in/sites -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/references
+    find ~/in/reference_genome -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/references
+    find ~/in/reference_index -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/references
+    
+    mark-section "Installing packages"
+    sudo -H python3 -m pip install --no-index --no-deps packages/*
 
-    # The following line(s) use the dx command-line tool to download your file
-    # inputs to the local file system using variable names for the filenames. To
-    # recover the original filenames, you can use the output of "dx describe
-    # "$variable" --name".
+    # run IGV reports
 
-    dx download "$sites" -o sites
-
-    dx download "$reference_genome" -o reference_genome
-
-    dx download "$reference_index" -o reference_index
-
-    # Fill in your application code here.
-    #
-    # To report any recognized errors in the correct format in
-    # $HOME/job_error.json and exit this script, you can use the
-    # dx-jobutil-report-error utility as follows:
-    #
-    #   dx-jobutil-report-error "My error message"
-    #
-    # Note however that this entire bash script is executed with -e
-    # when running in the cloud, so any line which returns a nonzero
-    # exit code will prematurely exit the script; if no error was
-    # reported in the job_error.json file, then the failure reason
-    # will be AppInternalError with a generic error message.
-
-    # The following line(s) use the dx command-line tool to upload your file
-    # outputs after you have created them on the local file system.  It assumes
-    # that you have used the output field name for the filename for each output,
-    # but you can change that behavior to suit your needs.  Run "dx upload -h"
-    # to see more options to set metadata.
-
+    # move results and output
     report=$(dx upload report --brief)
-
-    # The following line(s) use the utility dx-jobutil-add-output to format and
-    # add output variables to your job's output as appropriate for the output
-    # class.  Run "dx-jobutil-add-output -h" for more information on what it
-    # does.
 
     dx-jobutil-add-output report "$report" --class=file
 }
