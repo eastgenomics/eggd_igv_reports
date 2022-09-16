dde71bd99bd004f1973a4951b6f83557823ee8d5
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-09-16 13:00:52 +0100

    Initial commit

---
+++ src/code.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+main() {
+
+    echo "Value of sites: '$sites'"
+    echo "Value of reference_genome: '$reference_genome'"
+    echo "Value of reference_index: '$reference_index'"
+
+    # The following line(s) use the dx command-line tool to download your file
+    # inputs to the local file system using variable names for the filenames. To
+    # recover the original filenames, you can use the output of "dx describe
+    # "$variable" --name".
+
+    dx download "$sites" -o sites
+
+    dx download "$reference_genome" -o reference_genome
+
+    dx download "$reference_index" -o reference_index
+
+    # Fill in your application code here.
+    #
+    # To report any recognized errors in the correct format in
+    # $HOME/job_error.json and exit this script, you can use the
+    # dx-jobutil-report-error utility as follows:
+    #
+    #   dx-jobutil-report-error "My error message"
+    #
+    # Note however that this entire bash script is executed with -e
+    # when running in the cloud, so any line which returns a nonzero
+    # exit code will prematurely exit the script; if no error was
+    # reported in the job_error.json file, then the failure reason
+    # will be AppInternalError with a generic error message.
+
+    # The following line(s) use the dx command-line tool to upload your file
+    # outputs after you have created them on the local file system.  It assumes
+    # that you have used the output field name for the filename for each output,
+    # but you can change that behavior to suit your needs.  Run "dx upload -h"
+    # to see more options to set metadata.
+
+    report=$(dx upload report --brief)
+
+    # The following line(s) use the utility dx-jobutil-add-output to format and
+    # add output variables to your job's output as appropriate for the output
+    # class.  Run "dx-jobutil-add-output -h" for more information on what it
+    # does.
+
+    dx-jobutil-add-output report "$report" --class=file
+}
