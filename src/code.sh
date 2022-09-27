69d6006c2751ebee79f3326f3de9099bf194f2b6
corbin-chris <chris.corbin@addenbrookes.nhs.uk>
2022-09-27 18:42:25 +0100

    App now iterates over array of BAMs.

--- src/code.sh
+++ src/code.sh
@@ -9,25 +9,29 @@ main() {
     mkdir references
     dx-download-all-inputs --parallel
 
-    # BAMs put in same directory as their index
-    bam_file_name=$(find ~/in/bam_file -type f -name "*" -print0) 
-    sample_id=$(echo "$bam_file_name" | cut -d "." -f 1 )
+    # BAMs and BAIs put in same directory as their index
     find ~/in/bam_file -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/bam
     find ~/in/bam_index -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/bam
+
+    #TODO sense-check the BAM and BAI names
 
     # Put reference material in one dir
     bed_name=$(find ~/in/sites -type f -name "*" -print0)
     find ~/in/sites -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/references
 
-    mark-section "Installing packages"
+    mark-section "Installing igv-reports and packages from wheels"
     sudo -H python3 -m pip install --no-index --no-deps packages/*
 
-    mark-section "Run IGV reports"
-    create_report  ~/references/"$bed_name" \
-    --genome "$reference_genome" \
-    --flanking 1000 \
-    --tracks ~/bam/"$bam_file_name" \
-    --output ~/out/igv_reports/"$sample_id".html
+    mark-section "Generate an IGV report for each sample"
+    while IFS= read -r -d '' bam_file_name
+    do
+        sample_id=$(echo "$bam_file_name" | cut -d "." -f 1 )
+        create_report  ~/references/"$bed_name" \
+        --genome "$reference_genome" \
+        --flanking 1000 \
+        --tracks ~/bam/"$bam_file_name" \
+        --output ~/out/igv_reports/"$sample_id"_igv_report.html
+    done < $(find ~/in/bam_files -type f -name "*" -print0)
 
     mark-section "Output the HTML file"
     output_xlsx=$(dx upload /home/dnanexus/out/igv_reports/* --brief)
